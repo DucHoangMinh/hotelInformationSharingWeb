5d734a3914c1c9c2a1acadeb153485aaa29b4579
DucHoangMinh <hoangminhduc4125@gmail.com>
2023-10-12 11:08:57 +0700

    sử dụng @ControllerAdvice thêm trang error khi có exception

--- backend/src/main/java/com/example/backend/controller/LoginController.java
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.exception.UserException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +11,10 @@ public class LoginController {
     @GetMapping(path = "/")
     public String sendSuccessSignal(){
         return "Test Login Controller Successfully!";
+    }
+
+    @GetMapping(path = "/foo")
+    public String foo(){
+        throw new UserException("Some Error");
     }
 }
